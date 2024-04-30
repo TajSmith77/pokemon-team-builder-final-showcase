840197d84375e547521f9eba16a5bcc142ef3d18
Taj Smith <TajSmith@Deloitte.com>
2024-04-30 18:27:03 -0400

    Created user accounts and authentication and implemented it fully. Created teams page(WIP). Updated tests for all functionality. Added home page image. Organized filter form.

--- pokemon_team_builder/pokemon/tests/test_selenium.py
+++ pokemon_team_builder/pokemon/tests/test_selenium.py
@@ -1,28 +1,97 @@
-#test the functionality of my pokemon application
+# Test the functionality of the pokemon application
+import unittest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
 
-# Keep chrome open after program finishes
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_experimental_option("detach", True)
 
-driver = webdriver.Chrome(options=chrome_options)
-driver.get("http://127.0.0.1:8000")
+class TestPokemonApp(unittest.TestCase):
 
-pokemon_nav_button = driver.find_element(By.LINK_TEXT, "POKEMON")
-if (pokemon_nav_button.text == "POKEMON"):
-    pokemon_nav_button.click()
+    def setUp(self):
+        self.driver = webdriver.Chrome(options=webdriver.ChromeOptions().add_experimental_option("detach", True))
+        self.driver.get("http://127.0.0.1:8000")
 
-time.sleep(1)
+    def tearDown(self):
+        self.driver.quit()
 
-bulbasaur_button = driver.find_element(By.XPATH, "/html/body/div/ul[1]/li/a")
-if bulbasaur_button:
-    bulbasaur_button.click()
+    def test_pokemon_nav_button(self):
+        nav_button = self.driver.find_element(By.LINK_TEXT, "POKEMON")
+        nav_button.click()
+        self.assertEqual(self.driver.title, "Pokedex")
 
-time.sleep(3)
+    def test_bulbasaur_button(self):
+        nav_button = self.driver.find_element(By.LINK_TEXT, "POKEMON")
+        nav_button.click()
+        time.sleep(1)
+        bulbasaur_button = self.driver.find_element(By.TAG_NAME, "li")
+        bulbasaur_button.click()
+        self.assertEqual(self.driver.title, "bulbasaur Details")
 
-home_button = driver.find_element(By.LINK_TEXT, "HOME")
-if (home_button.text == "HOME"):
-    home_button.click()
+    def test_home_button(self):
+        nav_button = self.driver.find_element(By.LINK_TEXT, "POKEMON")
+        nav_button.click()
+        time.sleep(1)
+        bulbasaur_button = self.driver.find_element(By.TAG_NAME, "li")
+        bulbasaur_button.click()
+        time.sleep(3)
+        home_button = self.driver.find_element(By.LINK_TEXT, "HOME")
+        home_button.click()
+        self.assertEqual(self.driver.title, "Pokemon Team Builder")
+
+    def test_teams_nav_button(self):
+        nav_button = self.driver.find_element(By.LINK_TEXT, "TEAMS")
+        nav_button.click()
+        time.sleep(3)
+        self.assertEqual(self.driver.title, "Login")
+        username_input = self.driver.find_element(By.NAME, "username")
+        username_input.send_keys("ogtestuser")
+        password_input = self.driver.find_element(By.NAME, "password")
+        password_input.send_keys("password1!")
+        password_input.send_keys(Keys.RETURN)
+        time.sleep(3)
+        self.assertEqual(self.driver.title, "Pokemon Team Builder")
+        self.assertEqual(self.driver.find_element(By.LINK_TEXT, "LOGOUT").text, "LOGOUT")
+        nav_button = self.driver.find_element(By.LINK_TEXT, "TEAMS")
+        nav_button.click()
+        time.sleep(1)
+        self.assertEqual(self.driver.title, "Teams")
+
+
+    def test_login_button(self):
+        login_button = self.driver.find_element(By.LINK_TEXT, "LOGIN")
+        login_button.click()
+        self.assertEqual(self.driver.title, "Login")
+
+    def test_logging_in(self):
+        login_button = self.driver.find_element(By.LINK_TEXT, "LOGIN")
+        login_button.click()
+        username_input = self.driver.find_element(By.NAME, "username")
+        username_input.send_keys("ogtestuser")
+        password_input = self.driver.find_element(By.NAME, "password")
+        password_input.send_keys("password1!")
+        password_input.send_keys(Keys.RETURN)
+        time.sleep(3)
+        self.assertEqual(self.driver.title, "Pokemon Team Builder")
+        self.assertEqual(self.driver.find_element(By.LINK_TEXT, "LOGOUT").text, "LOGOUT")
+
+    def test_logout(self):
+        login_button = self.driver.find_element(By.LINK_TEXT, "LOGIN")
+        login_button.click()
+        username_input = self.driver.find_element(By.NAME, "username")
+        username_input.send_keys("ogtestuser")
+        password_input = self.driver.find_element(By.NAME, "password")
+        password_input.send_keys("password1!")
+        password_input.send_keys(Keys.RETURN)
+        time.sleep(3)
+        logout_button = self.driver.find_element(By.LINK_TEXT, "LOGOUT")
+        logout_button.click()
+        self.assertEqual(self.driver.title, "Login")
+        self.assertEqual(self.driver.find_element(By.LINK_TEXT, "LOGIN").text, "LOGIN")
+
+
+if __name__ == "__main__":
+    loader = unittest.TestLoader()
+    suite = loader.loadTestsFromTestCase(TestPokemonApp)
+    runner = unittest.TextTestRunner(verbosity=2)
+    runner.run(suite)
--- pokemon_team_builder/pokemon/urls.py
+++ pokemon_team_builder/pokemon/urls.py
@@ -1,9 +1,19 @@
 from django.urls import path
+from django.contrib import admin
 from . import views
+from django.conf import settings
+
 
 urlpatterns = [
-    path('', views.main, name='main'),
+    path('', views.main_redirect, name='main_redirect'),
+    path('home/', views.main, name='main'),
     path('pokemon/', views.pokemon, name='pokemon'),
     path('pokemon/details/<int:id>/', views.details, name='details'),
+    path('teams/', views.teams, name='teams'),
     path('testing/', views.testing, name='testing'),
+    path('accounts/login/', views.login_redirect, name='login_redirect'),
+    path('login/', views.login_page, name='login'),
+    path('register/',views.register_page, name='register'),
+    path('logout/', views.logout_page, name='logout'),
+    path('admin/', admin.site.urls),
 ]
